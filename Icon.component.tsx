6c1b1875a43bb49d989be2790c29d7e17165e438
Inplex-sys <Inplex-sys+VBTuMirEZrscYlFx@protonmail.ch>
2023-11-04 14:34:21 +0100

    Added Flip and Rotate

--- Icon.component.tsx
+++ Icon.component.tsx
@@ -4,6 +4,8 @@ type Props = {
     type: "solid" | "regular" | "light" | "brands" | "thin" | "duotone",
     icon: string,
     size?: string,
+    flip?: "horizontal" | "vertical",
+    rotate?: 90 | 180 | 270,
     className?: string
 }
 
@@ -12,10 +14,13 @@ export default class Icon extends Component<Props> {
         const type: string = "fa-" + this.props.type + " ";
         const icon: string = "fa-" + this.props.icon;
         const size: string = (this.props.size) ? " " + "fa-" + this.props.size : ""
+        const flip: string = (this.props.flip) ? " " + "fa-flip-" + this.props.flip : ""
+        const rotate: string = (this.props.rotate) ? " " + "fa-rotate-" + this.props.rotate : ""
+
         const className: string = (this.props.className) ? " " + this.props.className : ""
 
         return (
-            <i className={type + icon + size + className}></i>
+            <i className={type + icon + size + flip + rotate + className}></i>
         )
     }
 }
